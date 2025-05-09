fe4da16a2bbe40dcfcca50a0638ec92e03c9734f
markosavic993 <savicmarko993@gmail.com>
2025-05-09 17:00:26 +0200

    initial commit

--- bin/video-upload-service.ts
+++ bin/video-upload-service.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { VideoUploadServiceStack } from '../lib/video-upload-service-stack';
 
@@ -15,7 +14,7 @@ new VideoUploadServiceStack(app, 'VideoUploadServiceStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { region: 'eu-central-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
