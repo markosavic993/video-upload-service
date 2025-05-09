31018c762c601613f05e506494b4808adb377fff
Marko Savic <marko.savic@homegate.ch>
2025-05-09 16:33:22 +0200

    Initial commit

---
+++ test/video-upload-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VideoUploadService from '../lib/video-upload-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/video-upload-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VideoUploadService.VideoUploadServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
